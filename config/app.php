59f72559e30f0a6252ad119b4939070b4f2ce2d1
huangshoulin <huangshoulin@huangshoulindeiMac-Pro.local>
2021-07-11 21:58:26 +0800

    .

--- config/app.php
+++ config/app.php
@@ -19,7 +19,7 @@ return [
     // 应用地址
     'app_host'               => '',
     // 应用调试模式
-    'app_debug'              => false,
+    'app_debug'              => true,
     // 应用Trace
     'app_trace'              => false,
     // 是否支持多模块
@@ -137,7 +137,7 @@ return [
     'exception_tmpl'         => Env::get('think_path') . 'tpl/think_exception.tpl',
 
     // 错误显示信息,非调试模式有效
-    'error_message'          => '页面错误！请稍后再试～',
+    'error_message'          => '小伙子，别乱来。',
     // 显示错误信息
     'show_error_msg'         => false,
     // 异常处理handle类 留空使用 \think\exception\Handle
